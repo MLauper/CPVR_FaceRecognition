17b1f9d3c0957c80258b1a415d60aa556cea5542
mLauper <laupm4@bfh.ch>
2016-11-04 11:11:30 +0100

    Use relative paths

--- Extras/FaceRecognition - Image Gathering.ps1
+++ Extras/FaceRecognition - Image Gathering.ps1
@@ -1,3 +1,3 @@
-$direcotries = gci -Directory -Recurse D:\code\src\github.com\MLauper\CPVR_FaceRecognition\Images\cpvr_faces_160\ -Exclude .picasa*
+$direcotries = gci -Directory -Recurse D:\code\src\github.com\MLauper\CPVR_FaceRecognition\Images\cpvr_faces_320\ -Exclude .picasa*
 $pictures = $direcotries | % {gci $_ | select -first 10}
-$i = 1; foreach ($picture in $pictures) {Copy-Item $picture.FullName -Destination "C:\Users\Marco\Desktop\FLD_based Face Recognition System_v2\FLD_based Face Recognition System_v2\Train2\$i.jpg"; $i++ }
+$i = 1; foreach ($picture in $pictures) {Copy-Item $picture.FullName -Destination "D:\code\src\github.com\MLauper\CPVR_FaceRecognition\Samples\FLD_based Face Recognition System_v2\CustomTrainDatabase\$i.jpg"; $i++ }
