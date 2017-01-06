77c97279cfd8b531e2b2c91eb3875297e7b11095
MLauper <laupm4@bfh.ch>
2017-01-06 15:48:37 +0100

    make it once, make it right

--- ViolaJones_FaceDetection.m
+++ ViolaJones_FaceDetection.m
@@ -6,96 +6,78 @@
 % Date:       Nov-2016
 %##########################################################################
 
+% Define input and ouptut directories
 baseInputDir = '.\Images\cpvr_classes\';
 baseOutPutDir = '.\out\FaceDetection\ViolaJones\';
+
+% Switch to show detected face on source image
+showDetectedFaces = false;
+
+% Get input files and filter correctly
 files = dir(baseInputDir);
-% Get a logical vector that tells which is a directory.
 dirFlags = [files.isdir];
-% Extract only those that are directories.
 classFolders = files(dirFlags);
-% remove first two entries (it's always '.' and '..')
 classFolders(1:2) = [];
 
+% Define Mask used on output files
 outputMask = floor(imread('.\Images\mask_160.jpg')./255);
 
 for i = 1 : length(classFolders)    
     
+    % Create output base directory
     mkdir(strcat(baseOutPutDir,classFolders(i).name));
     
+    % List pictures
     groupPictureDirectory = strcat(baseInputDir,'\',classFolders(i).name);
     groupPictures = dir(groupPictureDirectory);
     groupPictures(1:2) = [];
+    
+    % Loop through each group picture
     for j = 1 : length(groupPictures)        
-        %create output directory for group picture
+        
+        % Create output directory
         mkdir(strcat(baseOutPutDir, classFolders(i).name,'\', groupPictures(j).name));
-         %Detect objects using Viola-Jones Algorithm
+        
+        % Read the input image
+        inputImg = imread(strcat(groupPictureDirectory,'\', groupPictures(j).name));
+
+        % Setup and execute Viola Jones Face Detector
         faceDetector = vision.CascadeObjectDetector;
         faceDetector.MinSize = [130 130];
-
-        %Read the input image        
-        inputImg = imread(strcat(groupPictureDirectory,'\', groupPictures(j).name));
-        
-        %Returns Bounding Box values based on number of objects
         boundingBoxes = step(faceDetector,inputImg);
 
         for k = 1:size(boundingBoxes,1)
+            % Retrieve current size of the detected face
             size_x = boundingBoxes(k,4);
             size_y = boundingBoxes(k,3);
             
-            % x / y = 320 / 240
-            % y is fixed
-            % desired_size_x = (320/240) * size_y; 
+            % Calculate desired sizes to comply with the aspect ration
             desired_size_x = (160/120) * size_y; 
             delta_size_x = desired_size_x - size_x;
             delta_correction_x = floor(delta_size_x / 2);
             
+            % Rewrite bounding box coordinates to comply with the aspect
+            % ration
             boundingBoxes(k,2) = floor(boundingBoxes(k,2) - delta_correction_x);
             boundingBoxes(k,4) = floor(boundingBoxes(k,4) + delta_size_x);
         end
         
-        %export images to filesystem
+        % Export face images to filesystem
         for k = 1:size(boundingBoxes,1)                  
             outputPath = strcat(baseOutPutDir,classFolders(i).name,'\', groupPictures(j).name, '\',sprintf('%02d.jpg', k));
-            %outputPath = sprintf(strcat(outputFolder,'%02d.jpg'),k);
-            
-            %outputPath  = sprintf('.\\out\\FaceDetection\\ViolaJones\\%02d.jpg',i);            
             outputImg = imresize(imcrop(inputImg, boundingBoxes(k,:)),[160,120]).*outputMask;
             imwrite(outputImg, outputPath);    
         end
 
-%         figure,
-%         imshow(inputImg); hold on
-%         for k = 1:size(boundingBoxes,1)   
-%             rectangle('Position',boundingBoxes(k,:),'LineWidth',5,'LineStyle','-','EdgeColor','r');      
-%         end
-%         title('Face Detection');
-%         hold off;
+        % If enabled, show detected faces on input image
+        if (showDetectedFaces == true)
+            figure,
+            imshow(inputImg); hold on
+            for k = 1:size(boundingBoxes,1)   
+                rectangle('Position',boundingBoxes(k,:),'LineWidth',5,'LineStyle','-','EdgeColor','r');      
+            end
+            title('Face Detection');
+            hold off;            
+        end
     end               
 end
-
-% classDirectories = dir('.\Images\cpvr_classes\');
-% for classDirectory = classDirectories
-%     if (classDirectory.isdir == 1)
-%         %outputDir = classDirectory.name;                
-%     end    
-% end
-
-
-
-
-%  IFaces = insertObjectAnnotation(I, 'rectangle', bboxes, 'Face');
-%  figure, imshow(IFaces), title('Detected faces');
-%  
-%  for i = 1:size(bboxes,1)
-%      rectangle('Position',BB(i,:),'LineWidth',5,'LineStyle','-','EdgeColor','r');
-%      imsize = size(image_data);    
-%  end
-          
-%Useful if not only the face should be detected
-%  bodyDetector = vision.CascadeObjectDetector('UpperBody'); 
-%  bodyDetector.MinSize = [30 30];
-%  bodyDetector.MergeThreshold = 10
-%  bboxBody = step(bodyDetector, I);
-
-%IBody = insertObjectAnnotation(I, 'rectangle',bboxBody,'Upper Body');
-%figure, imshow(IBody), title('Detected upper bodies');
